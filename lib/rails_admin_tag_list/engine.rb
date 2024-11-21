ee102ac0a234e8b7d0d49dca7b1abe1c537ff585
ignazio.laduca <125459391+Fireborn93@users.noreply.github.com>
2024-11-21 16:53:31 +0100

    Update engine.rb

--- lib/rails_admin_tag_list/engine.rb
+++ lib/rails_admin_tag_list/engine.rb
@@ -1,6 +1,6 @@
 module RailsAdminTagList
   class Engine < ::Rails::Engine
-    initializer 'rails_admin_rag_list.action_controller' do |app|
+    initializer 'rails_admin_tag_list.action_controller' do |app|
       ActiveSupport.on_load :action_controller do
         helper RailsAdminTagList::SuggestionsHelper
       end
